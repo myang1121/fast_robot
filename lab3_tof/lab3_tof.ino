553c2291d042171d3aa621bf2172dcb5a1f119ca
Michelle Yang <yangmichelle1121@gmail.com>
2026-02-24 04:23:14 -0500

    Add files via upload

--- lab3_tof/lab3_tof.ino
+++ lab3_tof/lab3_tof.ino
@@ -12,7 +12,6 @@
 
   Are you getting weird readings? Be sure the vacuum tape has been removed from the sensor.
 */
-
 #include <Wire.h>
 #include "SparkFun_VL53L1X.h" //Click here to get the library: http://librarymanager/All#SparkFun_VL53L1X
 
@@ -24,12 +23,135 @@ SFEVL53L1X distanceSensor;x
 //Uncomment the following line to use the optional shutdown and interrupt pins.
 //SFEVL53L1X distanceSensor(Wire, SHUTDOWN_PIN, INTERRUPT_PIN);
 
+//////////// Libraries for BLE ////////////
+#include "BLECStringCharacteristic.h"
+#include "EString.h"
+#include "RobotCommand.h"
+#include <ArduinoBLE.h>
+//////////// BLE UUIDs ////////////
+#define BLE_UUID_TEST_SERVICE "15bb5de7-5941-4ba2-bda0-784bb8817a1b"
+
+#define BLE_UUID_RX_STRING "9750f60b-9c9c-4158-b620-02ec9521cd99"
+
+#define BLE_UUID_TX_FLOAT "27616294-3063-4ecc-b60b-3470ddef2938"
+#define BLE_UUID_TX_STRING "f235a225-6735-4d73-94cb-ee5dfce9ba83"
+//////////// BLE Global Variables & lab 1 global variables ////////////
+BLEService testService(BLE_UUID_TEST_SERVICE);
+
+BLECStringCharacteristic rx_characteristic_string(BLE_UUID_RX_STRING, BLEWrite, MAX_MSG_SIZE);
+
+BLEFloatCharacteristic tx_characteristic_float(BLE_UUID_TX_FLOAT, BLERead | BLENotify);
+BLECStringCharacteristic tx_characteristic_string(BLE_UUID_TX_STRING, BLERead | BLENotify, MAX_MSG_SIZE);
+
+// RX
+RobotCommand robot_cmd(":|");
+
+//////////// BLE case statements, declare command and handle command ////////////
+enum CommandTypes
+{
+    GET_SENSOR1_DATA,
+};
+
+
+void handle_command()
+{   
+    // Set the command string from the characteristic value
+    robot_cmd.set_cmd_string(rx_characteristic_string.value(),
+                             rx_characteristic_string.valueLength());
+
+    bool success;
+    int cmd_type = -1;
+
+    // Get robot command type (an integer)
+    /* NOTE: THIS SHOULD ALWAYS BE CALLED BEFORE get_next_value()
+     * since it uses strtok internally (refer RobotCommand.h and 
+     * https://www.cplusplus.com/reference/cstring/strtok/)
+     */
+    success = robot_cmd.get_command_type(cmd_type);
+
+    // Check if the last tokenization was successful and return if failed
+    if (!success) {
+        return;
+    }
+
+    // Handle the command type accordingly
+    switch (cmd_type) {
+        /*
+         * Send ToF sensor 1 data to Jupyter Lab through ble
+         */
+        case GET_SENSOR1_DATA:
+           // through ble, send to python
+          tx_estring_value.clear();
+          tx_estring_value.append("Distance(mm):");
+          tx_estring_value.append(distance);
+          tx_estring_value.append(",T:");
+          tx_estring_value.append((int)millis());
+          tx_characteristic_string.writeValue(tx_estring_value.c_str()); //e.g. "Distance(mm):100,T:1234"
+          break;
+
+        /* 
+         * The default case may not capture all types of invalid commands.
+         * It is safer to validate the command string on the central device (in python)
+         * before writing to the characteristic.
+         */
+        default:
+            Serial.print("Invalid Command Type: ");
+            Serial.println(cmd_type);
+            break;
+    }
+}
+
+//////////// Start! ////////////
 void setup(void)
 {
   Wire.begin();
 
   Serial.begin(115200);
   Serial.println("VL53L1X Qwiic Test");
+
+
+  //////////// BLE Setup ////////////
+  BLE.begin();
+  // Set advertised local name and service
+    BLE.setDeviceName("Artemis BLE");
+    BLE.setLocalName("Artemis BLE");
+    BLE.setAdvertisedService(testService);
+
+    // Add BLE characteristics
+    testService.addCharacteristic(tx_characteristic_float);
+    testService.addCharacteristic(tx_characteristic_string);
+    testService.addCharacteristic(rx_characteristic_string);
+
+    // Add BLE service
+    BLE.addService(testService);
+
+    // Initial values for characteristics
+    // Set initial values to prevent errors when reading for the first time on central devices
+    tx_characteristic_float.writeValue(0.0);
+
+    /*
+     * An example using the EString
+     */
+    // Clear the contents of the EString before using it
+    tx_estring_value.clear();
+
+    // Append the string literal "[->"
+    tx_estring_value.append("[->");
+
+    // Append the float value
+    tx_estring_value.append(9.0);
+
+    // Append the string literal "<-]"
+    tx_estring_value.append("<-]");
+
+    // Write the value to the characteristic
+    tx_characteristic_string.writeValue(tx_estring_value.c_str());
+
+    // Output MAC Address
+    Serial.print("Advertising BLE with MAC: ");
+    Serial.println(BLE.address());
+
+    BLE.advertise();
 
   if (distanceSensor.begin() != 0) //Begin returns 0 on a good init
   {
