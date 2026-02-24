0d68315598107c3e2f25106895e4fab81637d5b9
Michelle Yang <yangmichelle1121@gmail.com>
2026-02-24 06:00:48 -0500

    working task 7

--- lab3_tof/lab3_tof.ino
+++ lab3_tof/lab3_tof.ino
@@ -1,99 +1,64 @@
 /*
-  Reading distance from the laser based VL53L1X
-  By: Nathan Seidle
-  SparkFun Electronics
-  Date: April 4th, 2018
-  License: This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
-
-  SparkFun labored with love to create this code. Feel like supporting open source hardware?
-  Buy a board from SparkFun! https://www.sparkfun.com/products/14667
-
-  This example prints the distance to an object.
-
-  Are you getting weird readings? Be sure the vacuum tape has been removed from the sensor.
+  Lab 3 - ToF Sensor with BLE
+  VL53L1X distance sensor in Long mode, streamed over BLE to Jupyter Lab.
 */
-#include <Wire.h>
-#include "SparkFun_VL53L1X.h" //Click here to get the library: http://librarymanager/All#SparkFun_VL53L1X
 
-//Optional interrupt and shutdown pins.
+#include <Wire.h>
+#include "SparkFun_VL53L1X.h"
+
 #define SHUTDOWN_PIN 2
 #define INTERRUPT_PIN 3
 
-SFEVL53L1X distanceSensor;x
-//Uncomment the following line to use the optional shutdown and interrupt pins.
-//SFEVL53L1X distanceSensor(Wire, SHUTDOWN_PIN, INTERRUPT_PIN);
+SFEVL53L1X distanceSensor; 
 
-//////////// Libraries for BLE ////////////
+//////////// BLE Libraries ////////////
 #include "BLECStringCharacteristic.h"
 #include "EString.h"
 #include "RobotCommand.h"
 #include <ArduinoBLE.h>
+
 //////////// BLE UUIDs ////////////
 #define BLE_UUID_TEST_SERVICE "15bb5de7-5941-4ba2-bda0-784bb8817a1b"
+#define BLE_UUID_RX_STRING    "9750f60b-9c9c-4158-b620-02ec9521cd99"
+#define BLE_UUID_TX_FLOAT     "27616294-3063-4ecc-b60b-3470ddef2938"
+#define BLE_UUID_TX_STRING    "f235a225-6735-4d73-94cb-ee5dfce9ba83"
 
-#define BLE_UUID_RX_STRING "9750f60b-9c9c-4158-b620-02ec9521cd99"
-
-#define BLE_UUID_TX_FLOAT "27616294-3063-4ecc-b60b-3470ddef2938"
-#define BLE_UUID_TX_STRING "f235a225-6735-4d73-94cb-ee5dfce9ba83"
-//////////// BLE Global Variables & lab 1 global variables ////////////
+//////////// BLE Globals ////////////
 BLEService testService(BLE_UUID_TEST_SERVICE);
-
 BLECStringCharacteristic rx_characteristic_string(BLE_UUID_RX_STRING, BLEWrite, MAX_MSG_SIZE);
-
-BLEFloatCharacteristic tx_characteristic_float(BLE_UUID_TX_FLOAT, BLERead | BLENotify);
+BLEFloatCharacteristic  tx_characteristic_float(BLE_UUID_TX_FLOAT, BLERead | BLENotify);
 BLECStringCharacteristic tx_characteristic_string(BLE_UUID_TX_STRING, BLERead | BLENotify, MAX_MSG_SIZE);
 
-// RX
 RobotCommand robot_cmd(":|");
+EString tx_estring_value;
 
-//////////// BLE case statements, declare command and handle command ////////////
-enum CommandTypes
-{
+// Declare distance as a global
+int distance = 0;
+
+//////////// Commands ////////////
+enum CommandTypes {
     GET_SENSOR1_DATA,
 };
 
-
-void handle_command()
-{   
-    // Set the command string from the characteristic value
+void handle_command() {
     robot_cmd.set_cmd_string(rx_characteristic_string.value(),
                              rx_characteristic_string.valueLength());
 
     bool success;
     int cmd_type = -1;
-
-    // Get robot command type (an integer)
-    /* NOTE: THIS SHOULD ALWAYS BE CALLED BEFORE get_next_value()
-     * since it uses strtok internally (refer RobotCommand.h and 
-     * https://www.cplusplus.com/reference/cstring/strtok/)
-     */
     success = robot_cmd.get_command_type(cmd_type);
+    if (!success) return;
 
-    // Check if the last tokenization was successful and return if failed
-    if (!success) {
-        return;
-    }
-
-    // Handle the command type accordingly
     switch (cmd_type) {
-        /*
-         * Send ToF sensor 1 data to Jupyter Lab through ble
-         */
         case GET_SENSOR1_DATA:
-           // through ble, send to python
-          tx_estring_value.clear();
-          tx_estring_value.append("Distance(mm):");
-          tx_estring_value.append(distance);
-          tx_estring_value.append(",T:");
-          tx_estring_value.append((int)millis());
-          tx_characteristic_string.writeValue(tx_estring_value.c_str()); //e.g. "Distance(mm):100,T:1234"
-          break;
+            tx_estring_value.clear();
+            tx_estring_value.append("Distance(mm):");
+            tx_estring_value.append(distance);
+            tx_estring_value.append(",T:");
+            tx_estring_value.append((int)millis());
+            tx_characteristic_string.writeValue(tx_estring_value.c_str());
+            break;
 
-        /* 
-         * The default case may not capture all types of invalid commands.
-         * It is safer to validate the command string on the central device (in python)
-         * before writing to the characteristic.
-         */
         default:
             Serial.print("Invalid Command Type: ");
             Serial.println(cmd_type);
@@ -101,86 +66,65 @@ void handle_command()
     }
 }
 
-//////////// Start! ////////////
-void setup(void)
-{
-  Wire.begin();
+//////////// Setup ////////////
+void setup(void) {
+    Wire.begin();
+    Serial.begin(115200);
+    Serial.println("VL53L1X Qwiic Test");
 
-  Serial.begin(115200);
-  Serial.println("VL53L1X Qwiic Test");
-
-
-  //////////// BLE Setup ////////////
-  BLE.begin();
-  // Set advertised local name and service
+    //////////// BLE Setup ////////////
+    BLE.begin();
     BLE.setDeviceName("Artemis BLE");
     BLE.setLocalName("Artemis BLE");
     BLE.setAdvertisedService(testService);
-
-    // Add BLE characteristics
     testService.addCharacteristic(tx_characteristic_float);
     testService.addCharacteristic(tx_characteristic_string);
     testService.addCharacteristic(rx_characteristic_string);
-
-    // Add BLE service
     BLE.addService(testService);
-
-    // Initial values for characteristics
-    // Set initial values to prevent errors when reading for the first time on central devices
     tx_characteristic_float.writeValue(0.0);
-
-    /*
-     * An example using the EString
-     */
-    // Clear the contents of the EString before using it
     tx_estring_value.clear();
-
-    // Append the string literal "[->"
-    tx_estring_value.append("[->");
-
-    // Append the float value
-    tx_estring_value.append(9.0);
-
-    // Append the string literal "<-]"
-    tx_estring_value.append("<-]");
-
-    // Write the value to the characteristic
+    tx_estring_value.append("[->"); tx_estring_value.append(9.0); tx_estring_value.append("<-]");
     tx_characteristic_string.writeValue(tx_estring_value.c_str());
-
-    // Output MAC Address
     Serial.print("Advertising BLE with MAC: ");
     Serial.println(BLE.address());
-
     BLE.advertise();
 
-  if (distanceSensor.begin() != 0) //Begin returns 0 on a good init
-  {
-    Serial.println("Sensor failed to begin. Please check wiring. Freezing...");
-    while (1)
-      ;
-  }
-  Serial.println("Sensor online!");
+    //////////// ToF Setup ////////////
+    if (distanceSensor.begin() != 0) {
+        Serial.println("Sensor failed to begin. Please check wiring. Freezing...");
+        while (1);
+    }
+    // Long mode, defaults, but just in case, call explicitly:
+    distanceSensor.setDistanceModeLong(); 
 }
 
-void loop(void)
-{
-  distanceSensor.startRanging(); //Write configuration bytes to initiate measurement
-  while (!distanceSensor.checkForDataReady())
-  {
-    delay(1);
-  }
-  int distance = distanceSensor.getDistance(); //Get the result of the measurement from the sensor
-  distanceSensor.clearInterrupt();
-  distanceSensor.stopRanging();
+//////////// Loop ////////////
+void loop(void) {
+    BLEDevice central = BLE.central();
 
-  Serial.print("Distance(mm): ");
-  Serial.print(distance);
+    if (central) {
+        Serial.print("Connected to: ");
+        Serial.println(central.address());
 
-  float distanceInches = distance * 0.0393701;
-  float distanceFeet = distanceInches / 12.0;
+        while (central.connected()) {
+            // Continuously take a fresh ranging measurement
+            distanceSensor.startRanging();
+            while (!distanceSensor.checkForDataReady()) {
+                delay(1);
+            }
+            distance = distanceSensor.getDistance(); // updates global
+            distanceSensor.clearInterrupt();
+            distanceSensor.stopRanging();
 
-  Serial.print("\tDistance(ft): ");
-  Serial.print(distanceFeet, 2);
+            Serial.print("Distance(mm): ");
+            Serial.println(distance);
 
-  Serial.println();
+            // Respond to any incoming BLE command
+            if (rx_characteristic_string.written()) {
+                handle_command();
+            }
+        }
+
+        Serial.println("Disconnected from central");
+    }
 }
